db36ac3e5b3b88cd5a0e3fe7bbed4c9e7996e5a8
Erik Dubois <erik.dubois@gmail.com>
2017-11-08 19:49:15 +0100

    extra software for i3

---
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	https://www.erikdubois.be
+# Website	:	https://www.archmerge.info
+# Website	:	https://www.archmerge.com
+# Website	:	https://www.archmerged.com
+# Website	:	https://www.archmergeforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# software from AUR (Arch User Repositories)
+# https://aur.archlinux.org/packages/
+
+#giving tmp folder extra gb in order not to run out of disk space while installing software
+#only if you run into issues with that
+#sudo mount -o remount,size=5G,noatime /tmp
+
+echo "DESKTOP SPECIFIC APPLICATIONS"
+
+echo "Installing category Accessories"
+
+echo "Installing category Development"
+
+echo "Installing category Education"
+
+echo "Installing category Games"
+
+echo "Installing category Graphics"
+
+echo "Installing category Internet"
+
+echo "Installing category Multimedia"
+
+echo "Installing category Office"
+
+echo "Installing category Other"
+
+echo "Installing category System"
+
+sudo pacman -S arandr --noconfirm --needed
+sudo pacman -S compton  --noconfirm --needed
+sudo pacman -S feh --noconfirm --needed
+sudo pacman -S gvfs  --noconfirm --needed
+sudo pacman -S lxappearance --noconfirm --needed
+sudo pacman -S nitrogen --noconfirm --needed
+sudo pacman -S notify-osd  --noconfirm --needed
+sudo pacman -S qt4 --confirm --needed
+sudo pacman -S volumeicon  --noconfirm --needed
+sudo pacman -S xorg-xrandr --noconfirm --needed
+
+echo "################################################################"
+echo "####    Software from Arch Linux Repository installed     ######"
+echo "################################################################"
