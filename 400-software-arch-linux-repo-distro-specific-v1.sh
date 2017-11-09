1a506fa3d31540a66ff8d9dc2c0cda3d36a49cd8
Erik Dubois <erik.dubois@gmail.com>
2017-11-09 14:36:04 +0100

    xfce4-taskmanager

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -61,6 +61,7 @@ sudo pacman -S xfce4-appfinder --noconfirm --needed
 sudo pacman -S xfce4-clipman-plugin --noconfirm --needed
 sudo pacman -S xfce4-settings --noconfirm --needed
 sudo pacman -S xfce4-screenshooter --noconfirm --needed
+sudo pacman -S xfce4-taskmanager --noconfirm --needed
 
 echo "################################################################"
 echo "####    Software from Arch Linux Repository installed     ######"
